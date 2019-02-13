7da411a3665be4fd44ea71d90bb379db9dcfcd4d
jmvillaescusa <jmvillaescusa99@gmail.com>
2019-02-13 16:54:35 -0500

    3

--- Chapter_2/Chapter_2/Chapter_2.cpp
+++ Chapter_2/Chapter_2/Chapter_2.cpp
@@ -1,11 +1,36 @@
 /*
 	Questions
+	1. 
 */
 
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	// Exercises
+    // Menu Chooser
+	std::cout << "Difficulty Levels\n\n";
+	std::cout << "1 - Easy\n";
+	std::cout << "2 - Normal\n";
+	std::cout << "3 - Hard\n\n";
+	
+	int choice;
+	std::cout << "Choice: ";
+	std::cin >> choice;
+
+	switch (choice) {
+	case 1:
+		std::cout << "You picked Easy.\n";
+		break;
+	case 2:
+		std::cout << "You picked Normal.\n";
+		break;
+	case 3:
+		std::cout << "You picked Hard.\n";
+		break;
+	default:
+		std::cout << "You made an illegal choice.\n";
+	}
+
 	return 0;
 }
