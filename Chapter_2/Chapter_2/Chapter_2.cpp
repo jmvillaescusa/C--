e4410c1933a42cfc276835d335f37316be1b94a6
jmvillaescusa <jmvillaescusa99@gmail.com>
2019-02-13 17:15:17 -0500

    4

--- Chapter_2/Chapter_2/Chapter_2.cpp
+++ Chapter_2/Chapter_2/Chapter_2.cpp
@@ -4,12 +4,14 @@
 */
 
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 int main()
 {
 	// Exercises
     // Menu Chooser
-	std::cout << "Difficulty Levels\n\n";
+	/*std::cout << "Difficulty Levels\n\n";
 	std::cout << "1 - Easy\n";
 	std::cout << "2 - Normal\n";
 	std::cout << "3 - Hard\n\n";
@@ -30,7 +32,31 @@ int main()
 		break;
 	default:
 		std::cout << "You made an illegal choice.\n";
-	}
+	}*/
+
+	// Guess My Number
+	/*srand(static_cast<unsigned int>(time(0)));
+	int secretNumber = rand() % 100 + 1;
+	int tries = 0;
+	int guess;
+
+	std::cout << "\nWelcome to Guess My Number\n";
+
+	do {
+		std::cout << "Enter a guess: ";
+		std::cin >> guess;
+		++tries;
+
+		if (guess > secretNumber) {
+			std::cout << "Too high!\n\n";
+		}
+		else if (guess < secretNumber) {
+			std::cout << "Too low!\n\n";
+		}
+		else {
+			std::cout << "\nThat's it! You got it in " << tries << " guesses!\n";
+		}
+	} while (guess != secretNumber);*/
 
 	return 0;
 }
