150446c2c6077f8415ff4e3ce8eeea1d8107cd85
jmvillaescusa <jmvillaescusa99@gmail.com>
2019-02-14 15:22:18 -0500

    a

--- Chapter_2/Chapter_2/Chapter_2.cpp
+++ Chapter_2/Chapter_2/Chapter_2.cpp
@@ -10,29 +10,45 @@
 int main()
 {
 	// Exercises
-    // Menu Chooser
+    // 1. Menu Chooser
 	/*std::cout << "Difficulty Levels\n\n";
 	std::cout << "1 - Easy\n";
 	std::cout << "2 - Normal\n";
 	std::cout << "3 - Hard\n\n";
 	
+	enum difficulty {EASY, NORMAL, HARD};
+	difficulty myDifficulty;
 	int choice;
 	std::cout << "Choice: ";
 	std::cin >> choice;
 
-	switch (choice) {
-	case 1:
+	if (choice == 1) {
+		myDifficulty = EASY;
+	}
+	else if (choice == 2) {
+		myDifficulty = NORMAL;
+	}
+	if (choice == 3) {
+		myDifficulty = HARD;
+	}
+
+	switch (myDifficulty) {
+	case EASY:
 		std::cout << "You picked Easy.\n";
 		break;
-	case 2:
+	case NORMAL:
 		std::cout << "You picked Normal.\n";
 		break;
-	case 3:
+	case HARD:
 		std::cout << "You picked Hard.\n";
 		break;
 	default:
 		std::cout << "You made an illegal choice.\n";
 	}*/
+
+	/*	2. - while(x) is a infinite loop
+	       - the code must have a garentee false statement for it to end the loop
+	       - example: while (x < 11) */
 
 	// Guess My Number
 	/*srand(static_cast<unsigned int>(time(0)));
@@ -57,6 +73,37 @@ int main()
 			std::cout << "\nThat's it! You got it in " << tries << " guesses!\n";
 		}
 	} while (guess != secretNumber);*/
+
+	// 3. Guess My Number 2.0
+	int low = 50;
+	int high = 100;
+	int guess;
+	int tries = 0;
+	int myNumber;
+
+	std::cout << "Welcome to Guess My Number 2.0!" << std::endl;
+	std::cout << "Please Enter a number from 1 - 100 so that I can guess it.\n" << std::endl;
+
+	std::cout << "Your number: ";
+	std::cin >> myNumber;
+	std::cout << "\n";
+
+	do {
+		++tries;
+		guess = high - low;
+		std::cout << guess << std::endl;
+
+		if (guess > myNumber) {
+
+		}
+		else if (guess < myNumber) {
+
+		}
+		else {
+			std::cout << "\nThat's it! I got it in " << tries << " guesses!\n";
+		}
+		
+	} while (guess != myNumber);
 
 	return 0;
 }
