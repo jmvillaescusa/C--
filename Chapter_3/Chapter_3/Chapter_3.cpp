8df4a2ee5fb662fcfdc517576ce07a3711ac0cc6
jmvillaescusa <jmvillaescusa99@gmail.com>
2019-02-15 16:06:42 -0500

    Ch3

---
+++ Chapter_3/Chapter_3/Chapter_3.cpp
@@ -0,0 +1,28 @@
+/*
+	Questions
+	1. Galaga:
+	   Ship - lives
+	        - movement
+			- fire
+	   Commander - launch
+	             - grappleBeam
+				 - pointValue
+
+	2. Much more efficient and easier to code compared to individual variables
+	   
+	3. Cannot increase/decrease size of array thereafter
+*/
+#include <iostream>
+
+int main()
+{
+	//Exercises
+	/*
+		2.
+	*/
+	/*
+		3.
+	*/
+
+	return 0;
+}
