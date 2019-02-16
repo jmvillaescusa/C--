453ada6e4a2e939726ac9646bae6ba77a66fa073
jmv <44001505+jmvillaescusa@users.noreply.github.com>
2019-02-16 03:38:18 -0500

    a

--- Chapter_3/Chapter_3/Chapter_3.cpp
+++ Chapter_3/Chapter_3/Chapter_3.cpp
@@ -4,24 +4,36 @@
 	   Ship - lives
 	        - movement
 			- fire
+			- score
 	   Commander - launch
 	             - grappleBeam
 				 - pointValue
+				 - movement
+	   Minion - launch
+			  - fire
+			  - pointValue
+			  - movement
 
 	2. Much more efficient and easier to code compared to individual variables
+	   Simply call a certain element by the index number associated with it
 	   
-	3. Cannot increase/decrease size of array thereafter
+	3. Cannot increase/decrease size of array after initialized
+
+	4.
 */
 #include <iostream>
 
 int main()
 {
 	//Exercises
+	// 1.
+
 	/*
-		2.
+	   2.
 	*/
+
 	/*
-		3.
+	   3.
 	*/
 
 	return 0;
